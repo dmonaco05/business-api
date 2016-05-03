1e4a171ce7e38cb8f8e5b6a0f48bb3d6268c9753
dominic monaco <dominicmonaco@dominics-Air.fios-router.home>
2016-05-03 13:45:13 -0400

    issues with multiple renders - wip

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -1,14 +1,16 @@
-class BusinessesController < ApplicationController
+class API::V1::BusinessesController < ApplicationController
+  # GET /businesses
   def index
     @businesses = Business.all
-    
+
     render json: @businesses, status: :ok
     paginate json: @businesses, per_page: 50
   end
 
+  # GET /businesses/#
   def show
     @business = Business.find(params[:id])
-    
+
     render json: @business, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, :defaults => {:format => :json} do
-    get "/businesses",   to: "businesses#index"
-    get "/business/:id", to: "businesses#show"
+    namespace :v1 do
+      get "/businesses",   to: "businesses#index"
+      get "/businesses/:id", to: "businesses#show"
+    end
   end
 end
